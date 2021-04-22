3608e543e7d1a95fd17e80f87415c6560c8abee4
jadecrtl <jadecortial@gmail.com>
2021-04-22 13:40:32 +0200

    Item02 creation_test_utilisateur

---
+++ MySQL/creation_test_utilisateur.sql
@@ -0,0 +1,10 @@
+USE ProjetRJ;
+
+INSERT INTO t_utilisateur (pseudonyme, adresse_mail, mot_de_passe, pouvoir) VALUES ('admin1', 'admin1@gmail.com', 'admin1', 'admin');
+INSERT INTO t_utilisateur (pseudonyme, adresse_mail, mot_de_passe, pouvoir) VALUES ('admin2', 'admin2@gmail.com', 'admin2', 'admin');
+INSERT INTO t_utilisateur (pseudonyme, adresse_mail, mot_de_passe, pouvoir) VALUES ('influe1', 'influe1@gmail.com', 'influe1', 'public');
+INSERT INTO t_utilisateur (pseudonyme, adresse_mail, mot_de_passe, pouvoir) VALUES ('influe2', 'influe2@gmail.com', 'influe2', 'public');
+INSERT INTO t_utilisateur (pseudonyme, adresse_mail, mot_de_passe, pouvoir) VALUES ('influe3', 'influe3@gmail.com', 'influe3', 'public');
+INSERT INTO t_utilisateur (pseudonyme, adresse_mail, mot_de_passe, pouvoir) VALUES ('jade1', 'jade1@gmail.com', 'jade1', 'prive');
+INSERT INTO t_utilisateur (pseudonyme, adresse_mail, mot_de_passe, pouvoir) VALUES ('jade2', 'jade2@gmail.com', 'jade2', 'prive');
+
