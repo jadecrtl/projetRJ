3365ed775c9ae7ae47d2dcf003e67f2daa51f5b3
jadecrtl <jadecortial@gmail.com>
2021-04-22 19:04:50 +0200

    création des relations et jeux de test

---
+++ MySQL/creation_test_relation.sql
@@ -0,0 +1,7 @@
+USE ProjetRJ;
+
+CALL ps_creer_abonnement('jade1', 'influe1');
+CALL ps_creer_abonnement('jade2', 'influe1');
+CALL ps_creer_abonnement('jade2', 'influe2');
+CALL ps_creer_abonnement('influe1', 'influe3');
+CALL ps_creer_abonnement('influe1', 'influe2');
---
+++ MySQL/ps_creer_abonnement.sql
@@ -0,0 +1,33 @@
+USE ProjetRJ;
+
+DROP PROCEDURE IF EXISTS ps_creer_abonnement;
+
+DELIMITER #
+
+CREATE PROCEDURE ps_creer_abonnement (IN p_pseudonyme_demandeur VARCHAR(40), IN p_pseudonyme_influenceur VARCHAR(40))
+BEGIN
+    DECLARE l_id_influenceur INT;
+    DECLARE l_id_demandeur INT;
+    DECLARE l_relation_existante INT;
+
+    SELECT @l_id_influenceur := id_utilisateur 
+    FROM t_utilisateur 
+    WHERE pseudonyme = p_pseudonyme_influenceur 
+    AND pouvoir = 'public';
+
+    SELECT @l_id_demandeur := id_utilisateur
+    FROM t_utilisateur 
+    WHERE pseudonyme = p_pseudonyme_demandeur;
+
+    SELECT @l_relation_existante := COUNT(*)
+    FROM t_utilisateur_relation
+    WHERE id_utilisateur_demandeur = l_id_demandeur
+    AND id_utilisateur_repondant = l_id_influenceur;
+
+    IF (@l_id_influenceur IS NOT NULL AND @l_id_demandeur IS NOT NULL AND @l_relation_existante = 0 AND @l_id_demandeur <> @l_id_influenceur) THEN    
+        INSERT INTO t_utilisateur_relation (id_utilisateur_demandeur, id_utilisateur_repondant, relation)
+        VALUES (@l_id_demandeur, @l_id_influenceur, 'ami');
+    END IF;
+END#
+
+DELIMITER ;
