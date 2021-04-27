b342253656507a3887143759782b4e0b074f8b78
jadecrtl <jadecortial@gmail.com>
2021-04-27 16:38:43 +0200

    création de la ps_creer_demande_ami

---
+++ MySQL/ps_creer_demande_ami.sql
@@ -0,0 +1,33 @@
+USE ProjetRJ;
+
+DROP PROCEDURE IF EXISTS ps_creer_demande_ami;
+
+DELIMITER #
+
+CREATE PROCEDURE ps_creer_demande_ami (IN p_pseudonyme_demandeur VARCHAR(40), IN p_pseudonyme_repondant VARCHAR(40))
+BEGIN
+    DECLARE l_id_demandeur INT;
+    DECLARE l_id_repondant INT;
+    DECLARE l_relation_existante INT;
+
+    SELECT @l_id_demandeur := id_utilisateur 
+    FROM t_utilisateur 
+    WHERE pseudonyme = p_pseudonyme_demandeur;
+
+    SELECT @l_id_repondant := id_utilisateur
+    FROM t_utilisateur 
+    WHERE pseudonyme = p_pseudonyme_demandeur
+    AND pouvoir = 'prive';
+
+    SELECT @l_relation_existante := COUNT(*)
+    FROM t_utilisateur_relation
+    WHERE id_utilisateur_demandeur = l_id_demandeur
+    AND id_utilisateur_repondant = l_id_repondant;
+
+    IF (@l_id_demandeur IS NOT NULL AND @l_id_repondant IS NOT NULL AND @l_relation_existante = 0 AND @l_id_demandeur <> @l_id_repondant) THEN    
+        INSERT INTO t_utilisateur_relation (id_utilisateur_demandeur, id_utilisateur_repondant, relation)
+        VALUES (@l_id_demandeur, @l_id_repondant, 'attente');
+    END IF;
+END#
+
+DELIMITER ;
