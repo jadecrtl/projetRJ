78740340bc884849fcd75c50e9f67e9dbf154337
Rayanamkt <maktout.rayana@gmail.com>
2021-04-30 11:58:18 +0200

    creation de test_visiteur

--- JeuDeTest/test_visiteur.php
+++ JeuDeTest/test_visiteur.php
@@ -1,9 +1,12 @@
 <?php
 
     include ("bdd.php");
-   
-    //visiteur rien a faire
-    
-    
+
+    $requete_sql = ' SELECT * FROM t_publication WHERE id_utilisateur IN (
+        SELECT pseudonyme FROM t_utilisateur WHERE pouvoir ="public" ) ';
+    echo $requete_sql;
+    $reponse_sql = $bdd->query($requete_sql);
+ 
+
 
 ?>
