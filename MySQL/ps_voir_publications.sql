431a0cfc1ef71d9b7c26892b96bf45053df17079
jadecrtl <jadecortial@gmail.com>
2021-05-01 18:20:03 +0200

    voir publication en utilisant vue_amis

--- MySQL/ps_voir_publications.sql
+++ MySQL/ps_voir_publications.sql
@@ -1,26 +1,23 @@
 USE ProjetRJ;
 
-DROP PROCEDURE IF EXISTS ps_voir_publications
+DROP PROCEDURE IF EXISTS ps_voir_publications;
 
 DELIMITER #
 
-CREATE PROCEDURE ps_voir_publications (IN p_pseudonyme VARCHAR(40), IN p_id_publication INT)
+CREATE PROCEDURE ps_voir_publications (IN p_pseudonyme VARCHAR(40))
 BEGIN
     DECLARE l_id_utilisateur INT;
-    DECLARE l_id_publication INT;
          
     SELECT @l_id_utilisateur := id_utilisateur 
     FROM t_utilisateur 
     WHERE pseudonyme = p_pseudonyme;
 
-    SELECT @l_id_publication := id_publication
-    FROM t_publication
-    WHERE id_publication = p_id_publication;
-
-
-
-
-
+    SELECT u.pseudonyme, p.texte_publication, p.date_creation
+    FROM t_publication p
+    JOIN t_utilisateur u ON (p.id_utilisateur = u.id_utilisateur)
+    WHERE p.id_utilisateur IN (SELECT id_utilisateur_ami FROM vue_amis WHERE id_utilisateur = @l_id_utilisateur)
+    AND p.statut_publication = 'publiee'
+    ORDER BY p.date_creation DESC;
 
 END#
 
