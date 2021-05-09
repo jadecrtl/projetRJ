04a97b575afe03b60c3ae4d52921ed4e0f2a3dd9
jadecrtl <jadecortial@gmail.com>
2021-05-09 17:43:28 +0200

    modification de changer_mdp

--- PHP/changer_mdp.php
+++ PHP/changer_mdp.php
@@ -1,6 +1,19 @@
 <?php
-include('header.php');
+if (!isset($_SESSION)) {
+    session_start();
+}
+if (isset($_SESSION['pseudonyme_connecte']) && !empty($_SESSION['pseudonyme_connecte'])) {
+    echo "<h1>Changement du mot de passe pour " . $_SESSION['pseudonyme_connecte'] . "</h1>";
+} else {
+    echo "Erreur : vous n'êtes pas connecté.</br>";
+    echo "Retourner à la page d'<a href=\"accueil.php\">accueil</a>";
+    exit();
+}
+
+
+
 ?>
+
 <form action="changer_mdp_verification.php" method="POST">
 
     <label>Nouveau mot de passe :</label>
@@ -11,4 +24,6 @@ include('header.php');
     <input type="password" name="mdp_nouveau_confirm"></input><br />
 
     <input type="submit" name="changer_mdp" value="Modifier le mot de passe"></input><br />
+    <p>Retourner à la page d'<a href="accueil.php">accueil</a></p>
 </form>
+
--- PHP/profil_et_publications.php
+++ PHP/profil_et_publications.php
@@ -1,96 +1,78 @@
 <?php
-    if (!isset($_SESSION)) {
-        session_start();
-    }
+if (!isset($_SESSION)) {
+    session_start();
+}
 ?>
-    <?php
-    ?>
-    <div>
-        <h1>Mon profil</h1>
-        <table>
-            
-            <tr>
-                <td> <img src="img/avatar.png" alt="image" width="100" height="100"> </td>
-                <td></td>
-                <!--EXTRA qu'on pourrait faire mais on va pas le faire MAIS a presenter-->
-                <td> <a href="#">Modifier votre avatar</a> </td>
-            </tr>
-            <tr>
-                <th scope="col">Pseudo :
-                <?php
-                include('bdd.php');
-                if (isset($_SESSION['pseudonyme_connecte']) && !empty($_SESSION['pseudonyme_connecte'])) {
-                    $requete = "CALL ps_voir_mon_profil('".$_SESSION['pseudonyme_connecte']."')";
-                    $resultat = mysqli_query($connexion,$requete);
 
-                    if (!$resultat) {
-                        echo mysqli_error($connexion);
-                        exit();
-                    }
-                    if (mysqli_num_rows($resultat) == 1) {
-                        $affiche = mysqli_fetch_assoc($resultat);
-                        echo $affiche['pseudonyme'];
-                    }
-                    else {
-                        mysqli_free_result($resultat);
-                        echo "erreur: profil introuvable ou multiple profil";
-                    }
-                }
-                else {
-                    echo " Cher visiteur, veuillez vous connecter ou vous inscrire.";
-                }
-                ?>
-                </th>
-                <br/>
-                <td> <a href="#">Modifier votre pseudo</a></td>
-                <br/>
-                <td> <a href="changer_mdp.php">Modifier votre mot de passe</a></td>
-            </tr>
-            <tr>
-                <td>Compte : </td>
-                <td>
-                <?php
-                    echo $affiche['pouvoir'];
-                    mysqli_free_result($resultat);
-                    mysqli_close($connexion);
-                ?>
-                </td>
-            </tr>
-        </table>
-    </div>
+<div>
+    <h1>Mon profil</h1>
+    <article id="profil_avatar">
+        <p><img src="img/avatar.png" alt="image" width="100" height="100"></p>
+        <p><a href="#">Modifier votre avatar (non fonctionnel)</a></p>
+    </article>
 
-    <div>
-        <h1>Mes Publications</h1>
+    <article id="profil_information">
+
         <?php
-            include('bdd.php');
-            if (isset($_SESSION['pseudonyme_connecte']) && !empty($_SESSION['pseudonyme_connecte'])) {
-        
-                $requete = "CALL ps_voir_publications_profil('".$_SESSION['pseudonyme_connecte']."')";
-                $resultat_publications = mysqli_query($connexion,$requete);
+        include('bdd.php');
+        if (isset($_SESSION['pseudonyme_connecte']) && !empty($_SESSION['pseudonyme_connecte'])) {
+            $requete = "CALL ps_voir_mon_profil('" . $_SESSION['pseudonyme_connecte'] . "')";
+            $resultat = mysqli_query($connexion, $requete);
 
-                if (!$resultat_publications) {
-                    echo mysqli_error($connexion);
-                    exit();
-                }
-        
-                $affiche_publications = mysqli_fetch_assoc($resultat_publications);
-                if (mysqli_num_rows($resultat_publications) == 0) {
-                    echo "Aucune publication.";
-                }
-                while($affiche_publications) {
-                    echo "<article>";
-                    echo "<h2>".$affiche_publications['pseudonyme']." le ".$affiche_publications['date_creation']."</h2>";
-                    echo $affiche_publications['texte_publication'];
-                    echo "</article>";
-                    $affiche_publications = mysqli_fetch_assoc($resultat_publications);
-                }
-                mysqli_free_result($resultat_publications);
-                mysqli_close($connexion);
+            if (!$resultat) {
+                echo mysqli_error($connexion);
+                exit();
             }
+            if (mysqli_num_rows($resultat) == 1) {
+                $affiche = mysqli_fetch_assoc($resultat);
+                echo "<p>Pseudo: " . $affiche['pseudonyme'] . "</p>";
+                echo "<p>Adresse: " . $affiche['adresse_mail'] . "</p>";
+                echo "<p>Pouvoir: " . $affiche['pouvoir'] . "</p>";
+            } else {
+                mysqli_free_result($resultat);
+                echo "erreur: profil introuvable ou multiple profil";
+            }
+        } else {
+            echo " Cher visiteur, veuillez vous connecter ou vous inscrire.";
+        }
         ?>
-    </div>
+        <p><a href="#">Modifier votre pseudo (non fonctionnel)</a></p>
+        <p><a href="changer_mdp.php">Modifier votre mot de passe</a></p>
+
+    </article>
+</div>
+
+<div>
+    <h1>Mes Publications</h1>
+    <?php
+    include('bdd.php');
+    if (isset($_SESSION['pseudonyme_connecte']) && !empty($_SESSION['pseudonyme_connecte'])) {
+
+        $requete = "CALL ps_voir_publications_profil('" . $_SESSION['pseudonyme_connecte'] . "')";
+        $resultat_publications = mysqli_query($connexion, $requete);
+
+        if (!$resultat_publications) {
+            echo mysqli_error($connexion);
+            exit();
+        }
+
+        $affiche_publications = mysqli_fetch_assoc($resultat_publications);
+        if (mysqli_num_rows($resultat_publications) == 0) {
+            echo "Aucune publication.";
+        }
+        while ($affiche_publications) {
+            echo "<article>";
+            echo "<h2>" . $affiche_publications['pseudonyme'] . " le " . $affiche_publications['date_creation'] . "</h2>";
+            echo $affiche_publications['texte_publication'];
+            echo "</article>";
+            $affiche_publications = mysqli_fetch_assoc($resultat_publications);
+        }
+        mysqli_free_result($resultat_publications);
+        mysqli_close($connexion);
+    }
+    ?>
+</div>
 
 </body>
+
 </html>
-
-
