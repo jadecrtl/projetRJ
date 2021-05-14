c14e784fd3d7442dba341af5970dc213bea43502
jadecrtl <jadecortial@gmail.com>
2021-05-14 13:57:37 +0200

    création des boutons publier et annuler

--- PHP/accueil_creer_publication.php
+++ PHP/accueil_creer_publication.php
@@ -9,7 +9,13 @@
 <body>
     <h1>Nouvelle publication sur mode-up</h1>
     <form action="creer_publication_verification.php" method="POST">
-        <textarea name="publication" rows="5" cols="40"></textarea>
+        <textarea name="publication" rows="5" cols="40" placeholder="Vous pouvez écrire ici."></textarea>
+    </form>
+    <form action="publication_publiee.php" method="POST">
+        <input type="submit" name="publier" value="Publier">
+    </form>
+    <form action="publication_annuler.php" method="POST">
+        <input type="submit" name="annuler" value="Annuler">
     </form>
 </body>
 
