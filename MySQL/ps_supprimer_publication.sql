1e2874243b4b65823722e530b42f1944af5c0cd4
Rayanamkt <maktout.rayana@gmail.com>
2021-05-15 14:53:45 +0200

    creation de suppression de publication en procedur

---
+++ MySQL/ps_supprimer_publication.sql
@@ -0,0 +1,23 @@
+USE ProjetRJ;
+
+DROP PROCEDURE IF EXISTS ps_supprimer_publication;
+
+DELIMITER #
+CREATE PROCEDURE ps_supprimer_publication(IN p_pseudonyme VARCHAR(40),p_id_publication INT)
+BEGIN 
+    DECLARE l_id_utilisateur INT;
+    DECLARE l_id_publication INT;
+
+    SET @l_id_utilisateur = (SELECT id_utilisateur FROM t_utilisateur WHERE pseudonyme = p_pseudonyme AND ((pouvoir = 'public') OR (pouvoir ='prive')) );
+
+    SET @l_id_publication = (SELECT id_publication FROM t_publication WHERE id_publication = p_id_publication AND statut_publication = 'publiee');
+
+    IF (@l_id_utilisateur IS NOT NULL AND @l_id_publication IS NOT NULL) THEN    
+    UPDATE t_publication
+    SET statut_publication = 'suprimee'
+    WHERE id_utilisateur = @l_id_utilisateur
+    AND id_publication = @l_id_publication;
+    END IF;
+
+END#
+DELIMITER ;
