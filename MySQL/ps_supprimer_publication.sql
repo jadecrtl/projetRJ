93694dc1719fa60e615666705544da68770265cc
jadecrtl <jadecortial@gmail.com>
2021-05-15 15:35:57 +0200

    maj du backlog et des procédures

--- MySQL/ps_supprimer_publication.sql
+++ MySQL/ps_supprimer_publication.sql
@@ -1,23 +1,24 @@
 USE ProjetRJ;
 
-DROP PROCEDURE IF EXISTS ps_supprimer_publication;
+DROP PROCEDURE IF EXISTS ps_supprimer_publications;
 
 DELIMITER #
-CREATE PROCEDURE ps_supprimer_publication(IN p_pseudonyme VARCHAR(40), IN p_id_publication INT)
-BEGIN 
-    DECLARE l_id_utilisateur INT;
-    DECLARE l_id_publication INT;
 
-    SET @l_id_utilisateur = (SELECT id_utilisateur FROM t_utilisateur WHERE pseudonyme = p_pseudonyme AND ((pouvoir = 'public') OR (pouvoir ='prive')) );
+CREATE PROCEDURE ps_supprimer_publications (IN p_pseudonyme VARCHAR(40), IN p_id_publication INT)
+BEGIN  
 
-    SET @l_id_publication = (SELECT id_publication FROM t_publication WHERE id_publication = p_id_publication AND statut_publication = 'publiee');
+    DECLARE l_pouvoir ENUM("admin", "prive", "public");
 
-    IF (@l_id_utilisateur IS NOT NULL AND @l_id_publication IS NOT NULL) THEN    
-    UPDATE t_publication
-    SET statut_publication = 'suprimee'
-    WHERE id_utilisateur = @l_id_utilisateur
-    AND id_publication = @l_id_publication;
+    SET @l_pouvoir = (SELECT pouvoir FROM t_utilisateur WHERE pseudonyme = p_pseudonyme);
+
+    IF (@l_pouvoir = "prive" OR @l_pouvoir = "public") THEN
+        CALL ps_supprimer_publication_prive_public(p_pseudonyme, p_id_publication);
+    END IF;
+
+    IF (@l_pouvoir = "admin") THEN
+        CALL ps_censurer_publications_admin(p_pseudonyme, p_id_publication);
     END IF;
 
 END#
+
 DELIMITER ;
