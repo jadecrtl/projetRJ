d0016d8b43dc82cdbf0a6ba6cc0900ec8cb66951
jadecrtl <jadecortial@gmail.com>
2021-05-21 21:32:25 +0200

    application du style

--- PHP/inscription.php
+++ PHP/inscription.php
@@ -4,47 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <title>Inscription</title>
-    <style>
-        .container {
-
-            /* On ajoute une marge sur les cotés de l'écran */
-            padding-right: 15px;
-            padding-left: 15px;
-
-            /* Et on centre */
-            margin-right: auto;
-            margin-left: auto;
-
-        }
-
-        @media (min-width: 1200px) {
-            .container {
-                width: 1170px;
-            }
-        }
-    </style>
+    <link rel="stylesheet" href="css/style.css" />
 </head>
 
-<body class="container">
+<body>
+    <div class="blocPrincipal">
+        <h1> Inscription : créer votre compte chez mode-up </h1>
+        <form action="inscription_verification.php" method="POST">
 
-    <h1> Inscription : créer votre compte chez mode-up </h1>
-    <form action="inscription_verification.php" method="POST">
+            <input type="text" name="saisie_pseudonyme" placeholder="Pseudo"> <br>
 
-        <label>Pseudo :</label>
-        <input type="text" name="saisie_pseudonyme"> <br>
+            <input type="text" name="saisie_adresse_mail" placeholder="Adresse mail"> <br>
 
-        <label>Adresse mail :</label>
-        <input type="text" name="saisie_adresse_mail"> <br>
+            <input type="password" name="saisie_mdp1" placeholder="Mot de passe"> <br>
 
-        <label>Mot de passe :</label>
-        <input type="password" name="saisie_mdp1"> <br>
+            <input type="password" name="saisie_mdp2" placeholder="Confirmer votre mot de passe"> <br>
 
-        <label>Confirmer votre mot de passe :</label>
-        <input type="password" name="saisie_mdp2"> <br>
+            <input type="submit" name="inscription" value="Inscription">
 
-        <input type="submit" name="inscription" value="Inscription">
-
-    </form>
+        </form>
+    </div>
 </body>
 
 </html>
